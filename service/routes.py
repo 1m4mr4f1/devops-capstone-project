9e8a4cbe62b64c8ab57e8490aabd228fd5d14ac1
1m4mr4f1 <imamrafi192005@gmail.com>
2026-04-08 20:05:43 -0400

    fix: finally fixed E302 linting issue

--- service/routes.py
+++ service/routes.py
@@ -50,10 +50,10 @@ def create_accounts():
     account.deserialize(request.get_json())
     account.create()
     message = account.serialize()
-    
+
     # URL diaktifkan karena get_accounts sudah diimplementasikan di bawah
     location_url = url_for("get_accounts", account_id=account.id, _external=True)
-    
+
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
@@ -61,6 +61,8 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -87,7 +89,7 @@ def get_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
+
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -104,7 +106,7 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
+
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
@@ -123,7 +125,7 @@ def delete_accounts(account_id):
     account = Account.find(account_id)
     if account:
         account.delete()
-    
+
     return "", status.HTTP_204_NO_CONTENT
 
 
